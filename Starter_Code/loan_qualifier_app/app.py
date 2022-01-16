ec20189768768b7a5cc10ba9811f6a676a292037
JohnBatarse <jbatarse@hotmail.com>
2022-01-15 19:20:40 -0800

    Update comments
    Added comments for users to follow logic of code.

--- Starter_Code/loan_qualifier_app/app.py
+++ Starter_Code/loan_qualifier_app/app.py
@@ -3,34 +3,32 @@
 
 This is a command line application to match applicants with qualifying loans.
 
-Example:
-    $ python app.py
 """
-# Initial Imports
 
+# Initial Imports
 import sys
 import csv
 import fire
 import questionary
 from pathlib import Path
 
-# 
+# Import loan_csv to read fileio.py file.
 from qualifier.utils.fileio import load_csv
 
-#
+# Imports ratio files for calculation qualifiers.
 from qualifier.utils.calculators import (
     calculate_monthly_debt_ratio,
     calculate_loan_to_value_ratio,
 )
 
-# Filters users variables to match with lenders criteria.
+# Imports Filter files for calculating qualifiers.
 from qualifier.filters.max_loan_size import filter_max_loan_size
 from qualifier.filters.credit_score import filter_credit_score
 from qualifier.filters.debt_to_income import filter_debt_to_income
 from qualifier.filters.loan_to_value import filter_loan_to_value
 
 # This functions requests location of data_rate_sheet from user.
-# Giving path to the csv; if no path found, then system exits app.py
+
 def load_bank_data():
     """Ask for the file path to the latest banking data and load the CSV file.
 
@@ -38,6 +36,7 @@ def load_bank_data():
         The bank data from the data rate sheet CSV file.
     """
 
+    # Give path to the csv file; if no path found, then system exits app.py.
     csvpath = questionary.text("Enter a file path to a rate-sheet (.csv):").ask()
     csvpath = Path(csvpath)
     if not csvpath.exists():
@@ -45,20 +44,21 @@ def load_bank_data():
 
     return load_csv(csvpath)
 
-#
+# This function interacts with Users. Requests variables needed to calculate qualifiers.
 def get_applicant_info():
     """Prompt dialog to get the applicant's financial information.
 
     Returns:
         Returns the applicant's financial information.
     """
-
+    # Questions for the Users to enter variables needed to calculate qualifications.
     credit_score = questionary.text("What's your credit score?").ask()
     debt = questionary.text("What's your current amount of monthly debt?").ask()
     income = questionary.text("What's your total monthly income?").ask()
     loan_amount = questionary.text("What's your desired loan amount?").ask()
     home_value = questionary.text("What's your home value?").ask()
 
+    # Variable inputs from Users must be numbers to run calculations.
     credit_score = int(credit_score)
     debt = float(debt)
     income = float(income)
@@ -67,7 +67,8 @@ def get_applicant_info():
 
     return credit_score, debt, income, loan_amount, home_value
 
-#
+# This function calculates ratios and filters bank qualifications.
+# Prints the number of qualified loans found.
 def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_value):
     """Determine which loans the user qualifies for.
 
@@ -108,14 +109,16 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
 
     return bank_data_filtered
 
-
+# This function interacts with Users. Questions to save and name of file to save as csv.
+# If no loans qualify, then system will exit.
+# When loans qualify, the function will create a csv file, write the output and save file.
 def save_qualifying_loans(qualifying_loans):
     """Saves the qualifying loans to a CSV file.
 
     Args:
         qualifying_loans (list of lists): The qualifying bank loans.
     """
-   # This is the Usability dialog for savings the CSV Files.
+# This is the Usability dialog for savings the CSV Files.
 
     if not qualifying_loans:
         sys.exit("Sorry, no loans available")
@@ -136,6 +139,8 @@ def save_qualifying_loans(qualifying_loans):
     else:
         print("The qualifying loans list has not been saved.")
 
+
+# This function is the main execution point of the application. It triggers all the business logic.
 def run():
     """The main function for running the script."""
 
